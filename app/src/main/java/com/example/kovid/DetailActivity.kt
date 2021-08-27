58885e0a655e4a35965d2ecb4c7ab6389974bd42
pinocchio22 <vviian.2@gmail.com>
2021-08-27 18:06:19 +0900

    fix : getSerializableExtra
    getSerializableExtra -> getParcelableExtra

--- app/src/main/java/com/example/kovid/DetailActivity.kt
+++ app/src/main/java/com/example/kovid/DetailActivity.kt
@@ -1,27 +1,38 @@
 package com.example.kovid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detail.*
+
 
 @Suppress("CAST_NEVER_SUCCEEDS")
 class DetailActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        lateinit var datas : FacnameList
+//        lateinit var datas : FacnameList
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+        var intent : Intent = getIntent()
+        val datas: FacnameList? = intent.getParcelableExtra("data") as FacnameList?
 
 //        val recieve = intent.getStringExtra("data")
 //        text1.text = recieve
-
-        datas = intent.getSerializableExtra("data") as FacnameList
+//        datas = FacnameList("", "", "")
+//        datas = intent.getSerializableExtra("data") as FacnameList
+//        datas = (intent.getSerializableExtra("data") as? FacnameList)!!
+//        (intent.getSerializableExtra("data") as? FacnameList)
         Log.d("데이터2", datas.toString())
 
-        text1.text = datas.name
-        text2.text = datas.num
-        text3.text = datas.add
+        if (datas != null) {
+            text1.text = datas.name
+        }
+        if (datas != null) {
+            text2.text = datas.num
+        }
+        if (datas != null) {
+            text3.text = datas.add
+        }
     }
 }
